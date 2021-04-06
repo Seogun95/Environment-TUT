273c7239056a0c2295ef794a686471d45832e58c
SeonJungKim <ksj083895@gmail.com>
2021-04-06 10:56:06 +0500

    Environment TUT

--- Environment TUT/ContentView.swift
+++ Environment TUT/ContentView.swift
@@ -1,21 +1,61 @@
-//
-//  ContentView.swift
-//  Environment TUT
-//
-//  Created by 김선중 on 2021/04/06.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+// 1. 기본값으로 키 생성
+private struct CaptionColorKey: EnvironmentKey {
+  static let defaultValue = Color(.secondarySystemBackground)
+}
+
+// 2. 속성을 통해 환경 확장
+extension EnvironmentValues {
+  var captionBackgroundColor: Color {
+    get { self[CaptionColorKey.self] }
+    set { self[CaptionColorKey.self] = newValue }
+  }
+}
+
+// 3. 뷰 수정자 추가(선택사항)
+extension View {
+  func captionBackgroundColor(_ color: Color) -> some View {
+    environment(\.captionBackgroundColor, color)
+  }
+}
+// 4. Environment에 의존하여 배경색을 설정하는 수정자
+struct Caption: ViewModifier {
+  let font: Font
+  @Environment(\.captionBackgroundColor) var backgroundColor
+
+  func body(content: Content) -> some View {
+    content
+      .font(font)
+      .padding([.leading, .trailing], 5.0)
+      .background(backgroundColor)
+      .cornerRadius(5.0)
+  }
+}
+
+extension View {
+  func caption(font: Font = .caption) -> some View {
+    modifier(Caption(font: font))
+  }
+}
+
+
+// ContentView
+struct ContentView : View {
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("서근 개발블로그")
+          .caption(font: .largeTitle)
+          .captionBackgroundColor(.yellow)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
+                .previewLayout(.sizeThatFits)
+                
     }
 }
+
+
