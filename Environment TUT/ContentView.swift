84f114ad5bb665918ba39256a57fd8f48fdd9cca
SeonJungKim <ksj083895@gmail.com>
2021-04-06 10:55:27 +0500

    Initial Commit

---
+++ Environment TUT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Environment TUT
+//
+//  Created by 김선중 on 2021/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Environment TUT/Environment_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Environment_TUTApp.swift
+//  Environment TUT
+//
+//  Created by 김선중 on 2021/04/06.
+//
+
+import SwiftUI
+
+@main
+struct Environment_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
